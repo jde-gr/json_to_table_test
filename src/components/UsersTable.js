ce77f657a3ed2780029edbaeec09ec1f2a90806c
jde-gr <fjavier.delgado.esteban@gmail.com>
2022-07-13 13:14:16 +0200

    Step3-Applying styles to the table

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -12,11 +12,41 @@ const columns = Object.keys(data[0] || []).map((field) => ({
 console.log('Columns:');
 console.log(columns);
 
-// const tableGreen = '#01987A';
-// const tableBgGrey = '#F3F3F3';
+const tableGreen = '#01987A';
+const tableBgGrey = '#F3F3F3';
 
 const Styles = styled.div`
   padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    background-color: white;
+    border-bottom: 2px solid ${tableGreen};
+    border-radius: 8px 8px 0 0;
+    width: 80%;
+    margin: 1rem auto;
+    box-shadow: 0px -8px 8px 8px ${tableBgGrey};
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid #ccc;
+      text-align: left;
+    }
+
+    th {
+      background-color: ${tableGreen};
+      color: white;
+      font-weight: 700;
+      :first-child {
+        border-radius: 8px 0 0 0;
+      }
+      :last-child {
+        border-radius: 0 8px 0 0;
+      }
+    }
+  }
 `;
 
 const UsersTable = () => {
